11f2219fc62ec38e5ee99ba72b61ad63d9907ca7
Dini <khalil.dini@hotmail.fr>
2020-05-12 05:25:14 +0200

    First commit

---
+++ android/app/src/main/java/equilibre/com/chatwithflutter/MainActivity.java
@@ -0,0 +1,6 @@
+package equilibre.com.chatwithflutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './Controller/MainAppController.dart';
+import './Controller/LoginController.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: _handleAuth(),
+    );
+  }
+
+  Widget _handleAuth() {
+    return new StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (BuildContext context, snapshot){
+          if (snapshot.hasData){
+            //Authentified
+            return new MainAppController();
+          } else {
+            //Not authentified
+            return new LoginController();
+          }
+        }
+    );
+  }
+}
